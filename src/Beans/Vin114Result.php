e97df96c2c5d6b81235658291246e586e51f84ed
Benjamin Cao <caojianghui@gmail.com>
2017-02-08 19:18:35 -0600

    init repos

---
+++ src/Beans/Vin114Result.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Carnetmotors\Vin114\Beans;
+
+/**
+*
+*/
+class Vin114Result
+{
+    const SEPERATOR = ' | ';
+
+    private $levelId;           // LevelId  VIN对应的标识ID值
+    private $brand;             // PP  品牌
+    private $vehicleModel;      // CX  车型
+    private $vehicleLine;       // CXI  车系
+    private $manufacture;       // CJMC  厂家名称
+    private $saleName;          // XSMC  销售名称
+    private $guidePrice;        // ZDJG  指导价格
+    private $engineModel;       // FDJXH  发动机型号
+    private $cylinderNumber;    // FDJGS  缸数
+    private $displacement;      // PL  排量
+    private $enginePower;       // GL  发动机最大功率(kW)
+    private $fuelType;          // RYLX  燃油类型
+    private $fuelLevel;         // RYBH  燃油标号
+    private $dischargeStandard; // PFBZ  排放标准
+    private $transmissionType;  // BSQLX  变速器类型
+    private $gearboxDesc;       // BSQMS  变速器描述
+    private $driveMode;         // QDFS  驱动方式
+    private $gearNumber;        // DWS  档位数
+    private $doorNumber;        // CMS  车门数
+    private $seatNumber;        // ZWS  座位数
+    private $modelYear;         // NK  年款
+    private $vehicleBodyType;   // CSXS  车身形式
+    private $vehicleLevel;      // JB  车辆级别
+    private $vehicleType;       // CLLX  车辆类型
+    private $productYear;       // SCNF  生产年份
+    private $releaseYear;       // SSNF  上市年份
+    private $releaseMont;       // SSYF  上市月份
+    private $retirementYear;    // TCNF  停产年份
+    private $year;              // VINNF  VIN对应的年份，可为具体的年份和“未查到年代”两种
+    private $newLevelId;        // NLevelID  力洋标识ID（新编制ID）如：GBZ0142A0013
+
+    /**
+     * Magic method of __set.
+     *
+     * @param string $name
+     * @param any $value
+     */
+    public function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+
+    /**
+     * Magic method of __get.
+     *
+     * @param  string $name
+     *
+     * @return any or null
+     */
+    public function __get($name)
+    {
+        if (isset($this->$name)) {
+            return $this->$name;
+        }
+
+        return null;
+    }
+
+    public function __toString()
+    {
+        return '品牌: ' . $this->brand
+            . self::SEPERATOR . 'VIN对应的标识ID值：' . $this->levelId
+            . self::SEPERATOR . '品牌：' . $this->brand
+            . self::SEPERATOR . '车型：' . $this->vehicleModel
+            . self::SEPERATOR . '车系：' . $this->vehicleLine
+            . self::SEPERATOR . '厂家名称：' . $this->manufacture
+            . self::SEPERATOR . '销售名称：' . $this->saleName
+            . self::SEPERATOR . '指导价格：' . $this->guidePrice
+            . self::SEPERATOR . '发动机型号：' . $this->engineModel
+            . self::SEPERATOR . '缸数：' . $this->cylinderNumber
+            . self::SEPERATOR . '排量：' . $this->displacement
+            . self::SEPERATOR . '发动机最大功率(kW)：' . $this->enginePower
+            . self::SEPERATOR . '燃油类型：' . $this->fuelType
+            . self::SEPERATOR . '燃油标号：' . $this->fuelLevel
+            . self::SEPERATOR . '排放标准：' . $this->dischargeStandard
+            . self::SEPERATOR . '变速器类型：' . $this->transmissionType
+            . self::SEPERATOR . '变速器描述：' . $this->gearboxDesc
+            . self::SEPERATOR . '驱动方式：' . $this->driveMode
+            . self::SEPERATOR . '档位数：' . $this->gearNumber
+            . self::SEPERATOR . '车门数：' . $this->doorNumber
+            . self::SEPERATOR . '座位数：' . $this->seatNumber
+            . self::SEPERATOR . '年款：' . $this->modelYear
+            . self::SEPERATOR . '车身形式：' . $this->vehicleBodyType
+            . self::SEPERATOR . '车辆级别：' . $this->vehicleLevel
+            . self::SEPERATOR . '车辆类型：' . $this->vehicleType
+            . self::SEPERATOR . '生产年份：' . $this->productYear
+            . self::SEPERATOR . '上市年份：' . $this->releaseYear
+            . self::SEPERATOR . '上市月份：' . $this->releaseMont
+            . self::SEPERATOR . '停产年份：' . $this->retirementYear
+            . self::SEPERATOR . 'VIN对应的年份，可为具体的年份和“未查到年代”两种：' . $this->year
+            . self::SEPERATOR . '力洋标识ID（新编制ID）如：GBZ0142A0013：' . $this->newLevelId;
+    }
+}
---
+++ src/Client.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Carnetmotors\Vin114;
+
+/**
+*
+*/
+class Client
+{
+    const WEB_SERVICE_URL = 'http://58.221.57.73:8088/webService/BaInfoService.asmx?WSDL';
+
+    protected $errs = array(
+        'E6' => '力洋接口程序出现异常',
+        'E7' => 'IP验证不通过',
+        'E8' => '未查到对应数据',
+    );
+
+    private $keyParameterPair = array(
+        'LevelId'  => 'levelId',
+        'PP'       => 'brand',
+        'CX  '     => 'vehicleModel',
+        'CX'       => 'vehicleLine',
+        'CJMC'     => 'manufacture',
+        'XSMC'     => 'saleName',
+        'ZDJG'     => 'guidePrice',
+        'FDJXH'    => 'engineModel',
+        'FDJGS'    => 'cylinderNumber',
+        'PL'       => 'displacement',
+        'GL'       => 'enginePower',
+        'RYLX'     => 'fuelType',
+        'RYBH'     => 'fuelLevel',
+        'PFBZ'     => 'dischargeStandard',
+        'BSQLX'    => 'transmissionType',
+        'BSQMS'    => 'gearboxDesc',
+        'QDFS'     => 'driveMode',
+        'DWS'      => 'gearNumber',
+        'CMS'      => 'doorNumber',
+        'ZWS'      => 'seatNumber',
+        'NK'       => 'modelYear',
+        'CSXS'     => 'vehicleBodyType',
+        'JB'       => 'vehicleLevel',
+        'CLLX'     => 'vehicleType',
+        'SCNF'     => 'productYear',
+        'SSNF'     => 'releaseYear',
+        'SSYF'     => 'releaseMont',
+        'TCNF'     => 'retirementYear',
+        'VINNFVIN' => 'year',
+        'NLevelID' => 'newLevelId',
+    );
+
+    public function __construct($method)
+    {
+    }
+
+    public function fetchBaseInfo($vin, &$errMsg)
+    {
+        $vin114Result = null;
+
+        try {
+            $soapClient = new SoapClient(self::WEB_SERVICE_URL);
+            $response   = $soapClient->GetBaInfoByVIN(
+                array('vin' => $vin)
+            );
+
+            $errCodes = array_keys($errs);
+            if (in_array($errs, $response)) {
+                $errMsg = $errs[$response];
+                return null;
+            }
+
+            $json = json_decode($response);
+            if ($json == null) {
+                $errMsg = 'json decode error. response = ' . $response;
+                return null;
+            }
+
+            foreach ($keyParameterPair as $key => $value) {
+                $vin114Result->$value = $json->$key;
+            }
+        } catch (SOAPFault $fault) {
+            $errMsg = 'SOAPFault: ' . $fault->getMessage();
+            return null;
+        }
+
+        return $vin114Result;
+    }
+}
